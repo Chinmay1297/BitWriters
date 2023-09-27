f175d7d3083addf22f130c453a7bc965706da16d
Chinmay Prusty <kanha12297@gmail.com>
2023-09-27 23:44:55 +0530

    Added DbContext Class

---
+++ API/BitWriters.API/BitWriters.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BitWriters.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BitWriters.API.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
