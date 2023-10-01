7c34557d86ff2d05a3918c35ef6145a197b73b20
Chinmay Prusty <kanha12297@gmail.com>
2023-10-02 00:53:22 +0530

    Added update blogpost service on client side to talk to the api

--- UI/bitwriters/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ UI/bitwriters/src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, Subscription } from 'rxjs';
 import { BlogPostService } from '../services/blog-post.service';
 import { BlogPost } from '../models/blog-post.model';
 import { CategoryService } from '../../category/services/category.service';
 import { Category } from '../../category/models/category.model';
+import { UpdateBlogPost } from '../models/update-blog-post.model';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -15,13 +16,15 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
 
   id: string | null = null;
   routeSubscription?: Subscription;
+  updateBlogPostSubscription?: Subscription;
   blogPostModel?: BlogPost;
   categories$?: Observable<Category[]>;
   selectedCategories?: string[];
 
   constructor(private route: ActivatedRoute,
     private blogPostService: BlogPostService,
-    private categoryService: CategoryService) { }
+    private categoryService: CategoryService,
+    private router: Router) { }
 
 
   ngOnInit(): void {
@@ -36,7 +39,7 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
           this.blogPostService.getBlogPostById(this.id).subscribe({
             next: (response) => {
               this.blogPostModel = response;
-              this.selectedCategories = response.categories.map(x=>x.id);
+              this.selectedCategories = response.categories.map(x => x.id);
             }
           })
         }
@@ -44,13 +47,34 @@ export class EditBlogpostComponent implements OnInit, OnDestroy {
     })
   }
 
-  onFormSubmit(): void
-  {
+  onFormSubmit(): void {
+    //convert currentblogPostModel to Request Object
+    if (this.blogPostModel && this.id) {
+      let updateBlogPost: UpdateBlogPost = {
+        author: this.blogPostModel.author,
+        content: this.blogPostModel.content,
+        shortDescription: this.blogPostModel.shortDescription,
+        featuredImageUrl: this.blogPostModel.featuredImageUrl,
+        isVisible: this.blogPostModel.isVisible,
+        publishedDate: this.blogPostModel.publishedDate,
+        title: this.blogPostModel.title,
+        urlHandle: this.blogPostModel.urlHandle,
+        categories: this.selectedCategories ?? []
+      };
 
+      this.updateBlogPostSubscription = this.blogPostService.UpdateBlogPost(this.id, updateBlogPost).subscribe(
+        {
+          next: (response)=>{
+            this.router.navigateByUrl('/admin/blogposts');
+          }
+        }
+      )
+    }
   }
 
   ngOnDestroy(): void {
     this.routeSubscription?.unsubscribe();
+    this.updateBlogPostSubscription?.unsubscribe();
   }
 
 }
