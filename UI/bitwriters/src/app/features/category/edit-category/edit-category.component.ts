5551b9835d77028a17809f3ebde460609b78fe1f
Chinmay Prusty <kanha12297@gmail.com>
2023-10-01 12:43:51 +0530

    Added UI skeleton for edit category

---
+++ UI/bitwriters/src/app/features/category/edit-category/edit-category.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrls: ['./edit-category.component.css']
+})
+export class EditCategoryComponent implements OnInit, OnDestroy {
+
+  id: string | null = null;
+  paramsSubscription?: Subscription;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap.subscribe({
+      next: params => {
+        this.id = params.get('id');
+
+        if(this.id)
+        {
+          //get category data from api by id
+        }
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+  }
+
+}
