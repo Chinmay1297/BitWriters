bdbd02b5849e742a5c6779aa3c27f4d3211c247a
Chinmay Prusty <kanha12297@gmail.com>
2023-10-01 23:20:56 +0530

    Added multiselect category control to add blogpost screen.

--- UI/bitwriters/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ UI/bitwriters/src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
 import { BlogPostService } from '../services/blog-post.service';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { CategoryService } from '../../category/services/category.service';
+import { Category } from '../../category/models/category.model';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.css']
 })
-export class AddBlogpostComponent implements OnDestroy{
+export class AddBlogpostComponent implements OnDestroy, OnInit {
   blogModel: AddBlogPost;
   blogPostSubscription?: Subscription;
+  categories$?: Observable<Category[]>;
 
   constructor(private blogPostService: BlogPostService,
-    private router: Router) {
+    private router: Router,
+    private categoryService: CategoryService) {
     this.blogModel = {
       title: '',
       shortDescription: '',
@@ -23,14 +27,18 @@ export class AddBlogpostComponent implements OnDestroy{
       featuredImageUrl: '',
       author: '',
       isVisible: true,
-      publishedDate: new Date()
+      publishedDate: new Date(),
+      categories: []
     }
   }
-  
+  ngOnInit(): void {
+    this.categories$ = this.categoryService.getAllCategories();
+  }
+
   onFormSubmit(): void {
     this.blogPostSubscription = this.blogPostService.createBlogPost(this.blogModel)
       .subscribe({
-        next: (response)=>{
+        next: (response) => {
           this.router.navigateByUrl('/admin/blogposts');
         }
       });
