ff078a094c0c3873883945469cad2812fa934ada
Chinmay Prusty <kanha12297@gmail.com>
2023-10-02 12:37:30 +0530

    Added Image service to upload image file from form

--- UI/bitwriters/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/bitwriters/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageService } from './image.service';
 
 @Component({
   selector: 'app-image-selector',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class ImageSelectorComponent {
 
+  private file?: File;
+  fileName: string = '';
+  title: string = '';
+
+  constructor(private imageService: ImageService) { }
+
+  onFileUploadChange(event: Event): void {
+    const element = event.currentTarget as HTMLInputElement;
+    this.file = element.files?.[0];
+  }
+
+  uploadImage(): void {
+    if (this.file && this.fileName != '' && this.title != '') {
+      //Image service to upload image
+      this.imageService.uploadImage(this.file, this.fileName, this.title)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+          }
+        })
+    }
+  }
 }
---
+++ UI/bitwriters/src/app/shared/components/image-selector/image.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../models/blog-image.model';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor(private http: HttpClient) { }
+
+  uploadImage(file: File, fileName: string, title: string): Observable<BlogImage> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', fileName);
+    formData.append('title', title);
+
+    return this.http.post<BlogImage>(`${environment.apiBaseUrl}/api/images`, formData);
+  }
+}
