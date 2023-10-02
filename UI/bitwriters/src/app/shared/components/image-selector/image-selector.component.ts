bc31e55e18e34cebf029c277c67862ecddc87072
Chinmay Prusty <kanha12297@gmail.com>
2023-10-02 13:39:48 +0530

    Populated ImageSelector with getallimages response

--- UI/bitwriters/src/app/shared/components/image-selector/image-selector.component.ts
+++ UI/bitwriters/src/app/shared/components/image-selector/image-selector.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ImageService } from './image.service';
+import { Observable } from 'rxjs';
+import { BlogImage } from '../models/blog-image.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-image-selector',
   templateUrl: './image-selector.component.html',
   styleUrls: ['./image-selector.component.css']
 })
-export class ImageSelectorComponent {
+export class ImageSelectorComponent implements OnInit {
 
   private file?: File;
   fileName: string = '';
   title: string = '';
+  images$?: Observable<BlogImage[]>;
+
+  @ViewChild('form', { static: false }) imageUploadForm?: NgForm;
 
   constructor(private imageService: ImageService) { }
+
+  ngOnInit(): void {
+    this.getImages();
+  }
 
   onFileUploadChange(event: Event): void {
     const element = event.currentTarget as HTMLInputElement;
@@ -25,9 +35,15 @@ export class ImageSelectorComponent {
       this.imageService.uploadImage(this.file, this.fileName, this.title)
         .subscribe({
           next: (response) => {
-            console.log(response);
+            //console.log(response);
+            this.imageUploadForm?.resetForm();
+            this.getImages();
           }
         })
     }
+  }
+
+  private getImages() {
+    this.images$ = this.imageService.getAllImages();
   }
 }
